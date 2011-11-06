8cb84110a28558697a16b9a51d5cbc6039f36885
aviflombaum <avi@flombaum.com>
2011-11-06 15:35:18 -0500

    Made mixtape index default route, removed index.html in public, edited README

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Mixtapeapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'mixtapes#index'
 
   # See how all your routes lay out with "rake routes"
 
