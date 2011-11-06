0cd6819cc8cf412deaa9fe3aa17e7c4c04581bfc
aviflombaum <avi@flombaum.com>
2011-11-06 15:21:21 -0500

    Initial Rails 3.1 App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixtapeapp::Application.config.secret_token = 'bac4944d19d9e717fd9981f38fb742241812f03c579b333eb48f3cb7b892a006c7ed08a846ed9ba1a726d37965599bd4735029837dccac5eadfad4433d654f18'
