058b676dc370f5999c9c8276ec998d2502b463a9
aviflombaum <avi@flombaum.com>
2011-11-19 10:41:23 -0500

    Destroy a song from the mixtape edit page viaa checkbox

--- app/models/mixtape.rb
+++ app/models/mixtape.rb
@@ -3,7 +3,7 @@ class Mixtape < ActiveRecord::Base
   has_many :songs, :through => :playlists
   
   validates_presence_of :name
-  accepts_nested_attributes_for :songs, :reject_if => lambda { |m| m[:audio].blank? }
+  accepts_nested_attributes_for :songs, :reject_if => lambda { |m| m[:audio].blank? }, :allow_destroy => true
   
   # after_initialize(:on => :create) do
   #   self.songs.build if self.songs.blank?
