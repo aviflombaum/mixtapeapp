d2ba9ee7493142e5d5b69b73ed0244938b8d4376
aviflombaum <avi@flombaum.com>
2011-11-19 12:35:49 -0500

    basic position on mixtape edit form

--- app/models/mixtape.rb
+++ app/models/mixtape.rb
@@ -4,6 +4,7 @@ class Mixtape < ActiveRecord::Base
   
   validates_presence_of :name
   accepts_nested_attributes_for :songs, :reject_if => lambda { |m| m[:audio].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :playlists, :allow_destroy => true
   
   # after_initialize(:on => :create) do
   #   self.songs.build if self.songs.blank?
