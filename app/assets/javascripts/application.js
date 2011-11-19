1715707e4c6e91d15862900eba74aa5c9b4e3bb8
aviflombaum <avi@flombaum.com>
2011-11-19 11:31:29 -0500

    Refactored the mixtape.js a bit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,20 +7,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-$('form a.add-song').live('click', function() {
-  var content = $('#songs_fields_template').html();
-  var regexp  = new RegExp('new_songs', 'g');
-  var new_id  = new Date().getTime();
-      
-  $(this).before(content.replace(regexp, new_id)); 
-  return false;
-});
-
-$('form a.remove-song').live('click', function() {
-  var hidden_field = $(this).prev('input[type=hidden]')[0];
-  if(hidden_field) {
-    hidden_field.value = '1';
-  }
-  $(this).parents('.song.fields').hide();
-  return false;
-});
