c61890cc56a211a761ad2f0b8503155df9c8c425
aviflombaum <avi@flombaum.com>
2011-11-21 17:54:26 -0500

    User Mailer Spec fix and artists show fix

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "password_reset" do
+    let(:mail) { UserMailer.password_reset }
+  
+    it "renders the headers" do
+      mail.subject.should eq("Password reset")
+      mail.to.should eq(["to@example.org"])
+      mail.from.should eq(["from@example.com"])
+    end
+  
+    it "renders the body" do
+      mail.body.encoded.should match("Hi")
+    end
+  end
 end
