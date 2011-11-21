c1775c48a5194a4d9068d21a99c6e290bb7bfcee
aviflombaum <avi@flombaum.com>
2011-11-21 17:30:24 -0500

    Users now have secure passwords

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.1.1'
 
 gem 'sqlite3'
 
+gem 'bcrypt-ruby'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
+  validates :password, :presence => true, :on => :create
+  validates :password, :confirmation => true
   
 end
