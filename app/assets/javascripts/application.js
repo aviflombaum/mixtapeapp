4e8b3c3758577ff765e845e07b15470260125e22
aviflombaum <avi@flombaum.com>
2011-11-27 19:05:04 -0500

    Artist autocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require chosen.jquery.js
 //= require_tree .
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def self.names
+    order('name ASC').collect{|a| a.name}
+  end
   
 end
