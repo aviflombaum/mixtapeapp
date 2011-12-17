6e62a99efbf3c63804a2be7c0b784b8d99f728cb
aviflombaum <avi@flombaum.com>
2011-12-17 09:04:52 -0500

    share create action

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -3,4 +3,11 @@ class SharesController < ApplicationController
   def new
     @mixtape = Mixtape.find(params[:mixtape_id])
   end
+  
+  def create
+    @mixtape = Mixtape.find(params[:mixtape_id])
+    @user = User.find(params[:user_id])
+    @mixtape.add_user(@user, :listener)
+    redirect_to mixtape_url(@mixtape), :notice => "Shared mixtape with #{@user.username}"
+  end
 end
