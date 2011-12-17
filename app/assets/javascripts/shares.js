70c0e30006f06cfc5aee3005baf3144984c5e0df
aviflombaum <avi@flombaum.com>
2011-12-17 12:57:45 -0500

    autucomplete that works within thickbox

--- app/assets/javascripts/shares.js
+++ app/assets/javascripts/shares.js
@@ -1,11 +1,15 @@
 $(function(){
   $("a.share").click(function(e){
     $.get($(this).attr('href'), function(html){
-      $.fancybox(html);      
+      $.fancybox(html);
+
+      $("#user_id").autocomplete({
+    		source: "/users"
+    	});
     });
     e.preventDefault();
   });
-  
+
   // $("form#share_mixtape").live('submit', function(e){
   //   $.post($(this).attr("action"), $(this).serialize(), function(response){
   //     // setTimeout(function(){$.fancybox.close()}, 1800);
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   validates :password, :presence => true, :on => :create
   validates :password, :confirmation => true
   
+  acts_as_autocomplete :on => :username
+  
   def send_password_reset
     generate_token(:password_reset_token)
     self.password_reset_sent_at = Time.zone.now
