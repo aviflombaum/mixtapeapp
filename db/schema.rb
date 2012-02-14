a66e04e9e77692ad634ed7f97b181af5a3e08102
aviflombaum <avi@flombaum.com>
2012-02-13 19:52:38 -0500

    Add description to mixtape

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120213230533) do
+ActiveRecord::Schema.define(:version => 20120214000640) do
 
   create_table "mixtapes", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.text     "description"
   end
 
 end
