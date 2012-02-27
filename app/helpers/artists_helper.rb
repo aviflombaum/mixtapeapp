1e3995f4c17e71c54fe878fc9d832b0cf885033d
aviflombaum <avi@flombaum.com>
2012-02-27 17:49:06 -0500

    navigation and song partial fields and song

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,11 +1,5 @@
 module ArtistsHelper
   
-  def artist_song_count(artist)
-    if artist.song_count <= 0
-      "<td>NO SONGS!!!</td>"
-    else
-      "<td>#{artist.song_count}</td>"
-    end
-  end
+
   
 end
